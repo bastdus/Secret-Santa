0b743f1bad925d456fa0e3f8327d73a75a8323b1
Bastien Duseaux <bastien.duseaux@gmail.com>
2025-11-29 13:09:16 +0100

    fix: correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/secretsanta/",
+  base: "/Secret-Santa/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
